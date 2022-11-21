ab8aeb3e8e44ab7b1b98d2856ce8f05b102cb392
joice.joseph <joice.joseph@tothenew.com>
2022-11-21 21:56:14 +0530

    Admin API - implemented paging&sorting and added logging

--- src/main/java/com/ttn/ecommerce/controller/AdminController.java
+++ src/main/java/com/ttn/ecommerce/controller/AdminController.java
@@ -1,10 +1,11 @@
 package com.ttn.ecommerce.controller;
 
-import com.ttn.ecommerce.entity.Customer;
-import com.ttn.ecommerce.entity.User;
+
 import com.ttn.ecommerce.model.CustomerResponseDTO;
 import com.ttn.ecommerce.model.SellerResponseDTO;
 import com.ttn.ecommerce.service.AdminService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,38 +17,51 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/admin")
 public class AdminController {
+    Logger logger = LoggerFactory.getLogger(AdminController.class);
 
     @Autowired
     AdminService adminService;
 
-    @ResponseStatus(HttpStatus.OK)
+
     @GetMapping(path="/customers")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public List<CustomerResponseDTO> getAllCustomers(){
-        List<CustomerResponseDTO> customers = adminService.listAllCustomers();
-        return customers;
+    public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers(@RequestParam(defaultValue = "0") Integer pageNo,
+                                                                     @RequestParam(defaultValue = "10") Integer pageSize,
+                                                                     @RequestParam(defaultValue = "id") String sortBy){
+        logger.info("AdminController::getAllCustomers request body: " +pageNo+", "+pageSize+", "+sortBy );
+        List<CustomerResponseDTO> customers = adminService.listAllCustomers(pageNo, pageSize, sortBy);
+        logger.info("AdminController::getAllCustomers response: list of customers.");
+        return new ResponseEntity<List<CustomerResponseDTO>>(customers,HttpStatus.OK);
     }
 
-    @ResponseStatus(HttpStatus.OK)
+
     @GetMapping(path="/sellers")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public List<SellerResponseDTO> getAllSellers(){
-        List<SellerResponseDTO> sellers = adminService.listAllSellers();
-        return sellers;
+    public ResponseEntity<List<SellerResponseDTO>> getAllSellers(@RequestParam(defaultValue = "0") Integer pageNo,
+                                                                 @RequestParam(defaultValue = "10") Integer pageSize,
+                                                                 @RequestParam(defaultValue = "id") String sortBy){
+        logger.info("AdminController::getAllSellers request body: " +pageNo+", "+pageSize+", "+sortBy );
+        List<SellerResponseDTO> sellers = adminService.listAllSellers(pageNo, pageSize, sortBy);
+        logger.info("AdminController::getAllCustomers response: list of sellers.");
+        return new ResponseEntity<List<SellerResponseDTO>>(sellers,HttpStatus.OK);
     }
 
 
     @PatchMapping(path="/activate")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<String>  enableUser(@RequestParam("id") Long id){
+        logger.info("AdminController::enableUser request body: "+ id);
         String response = adminService.activateUser(id);
+        logger.info("AdminController::enableUser response : "+ response);
         return new ResponseEntity<String>(response,HttpStatus.OK);
     }
 
     @PatchMapping(path="/deactivate")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<String>  disableUser(@RequestParam("id") Long id){
-        String response = adminService.deActivateUser(id);
+        logger.info("AdminController::disableUser request body: "+ id);
+        String response = adminService.deactivateUser(id);
+        logger.info("AdminController::disableUser response : "+ response);
         return new ResponseEntity<String>(response,HttpStatus.OK);
     }
 
--- src/main/java/com/ttn/ecommerce/service/AdminService.java
+++ src/main/java/com/ttn/ecommerce/service/AdminService.java
@@ -9,7 +9,13 @@ import com.ttn.ecommerce.model.SellerResponseDTO;
 import com.ttn.ecommerce.repository.CustomerRepository;
 import com.ttn.ecommerce.repository.SellerRepository;
 import com.ttn.ecommerce.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,6 +27,7 @@ import java.util.Optional;
 @Service
 @RequestMapping(path = "api/admin")
 public class AdminService {
+    Logger logger = LoggerFactory.getLogger(AdminService.class);
 
     @Autowired
     UserRepository userRepository;
@@ -34,14 +41,18 @@ public class AdminService {
     @Autowired
     EmailService emailService;
 
-    public List<CustomerResponseDTO> listAllCustomers(){
-        List<Customer> customerList = customerRepository.findAll();
+
+    public List<CustomerResponseDTO> listAllCustomers(Integer pageNo, Integer pageSize, String sortBy){
+        logger.info("AdminService::listAllCustomers execution started.");
+
+        logger.debug("AdminService::listAllCustomers fetching list of customers");
+        Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+        Page<Customer> pagedResultCustomer = customerRepository.findAll(paging);
         List<CustomerResponseDTO> requiredCustomers = new ArrayList<>();
 
-        // null or empty list conditions
-        // paginate
 
-        for (Customer customer : customerList) {
+        logger.debug("AdminService::listAllCustomers converting customers to CustomerResponseDTO");
+        for (Customer customer : pagedResultCustomer) {
             User user = customer.getUser();
             CustomerResponseDTO customerResponseDTO = new CustomerResponseDTO();
             customerResponseDTO.setUserId(user.getId());
@@ -52,15 +63,23 @@ public class AdminService {
             requiredCustomers.add(customerResponseDTO);
         }
 
+        logger.debug("AdminService::listAllCustomers converting returning list of CustomerResponseDTO");
+
+        logger.info("AdminService::listAllCustomers execution ended.");
         return requiredCustomers;
     }
 
 
-    public List<SellerResponseDTO> listAllSellers(){
-        List<Seller> sellerList = sellerRepository.findAll();
+    public List<SellerResponseDTO> listAllSellers(Integer pageNo, Integer pageSize, String sortBy){
+        logger.info("AdminService::listAllSellers execution started.");
+
+        logger.debug("AdminService::listAllSellers fetching list of sellers");
+        Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+        Page<Seller> pagedResultSeller = sellerRepository.findAll(paging);
         List<SellerResponseDTO> requiredCustomers = new ArrayList<>();
 
-        for (Seller seller : sellerList) {
+        logger.debug("AdminService::listAllSellers converting sellers to SellerResponseDTO");
+        for (Seller seller : pagedResultSeller) {
             User user = seller.getUser();
             SellerResponseDTO sellerResponseDTO = new SellerResponseDTO();
             sellerResponseDTO.setUserId(user.getId());
@@ -75,45 +94,59 @@ public class AdminService {
             requiredCustomers.add(sellerResponseDTO);
         }
 
+        logger.debug("AdminService::listAllSellers returning list of SellerResponseDTO");
+
+        logger.info("AdminService::listAllSellers execution ended.");
         return requiredCustomers;
     }
 
 
     public String activateUser(Long userId){
+        logger.info("AdminService::activateUser execution started.");
         Optional<User> user = userRepository.findById(userId);
         if(user.isPresent()){
             if(user.get().isActive()){
+                logger.debug("AdminService::activateUser account already active");
+                logger.info("AdminService::activateUser execution ended.");
                 return "User is already active.";
             }
             else {
+                logger.debug("AdminService::activateUser activating account");
                 user.get().setActive(true);
                 userRepository.save(user.get());
                 emailService.sendIsActivatedMail(user.get());
+                logger.info("AdminService::activateUser execution ended.");
                 return "Account activated.";
             }
         }
         else{
+            logger.error("AdminService::activateUser exception occurred while activating account");
             // add exception in handler
             throw new UserNotFoundException("User not found.");
         }
 
     }
 
-    public String deActivateUser(Long userId){
+    public String deactivateUser(Long userId){
+        logger.info("AdminService::deactivateUser execution started.");
         Optional<User> user = userRepository.findById(userId);
-        if(user.get()!=null){
+        if(user.isPresent()){
             if(user.get().isActive()){
+                logger.debug("AdminService::deactivateUser deactivating account");
                 user.get().setActive(false);
                 userRepository.save(user.get());
                 emailService.sendDeActivatedMail(user.get());
+                logger.info("AdminService::deactivateUser execution ended.");
                 return "Account is deactivated.";
             }
             else {
-
+                logger.debug("AdminService::deactivateUser account already deactivated");
+                logger.info("AdminService::deactivateUser execution ended.");
                 return "Account is already de-activated.";
             }
         }
         else{
+            logger.error("AdminService::deactivateUser exception occurred while deactivating account");
             // add exception in handler
             throw new UserNotFoundException("User not found.");
         }
